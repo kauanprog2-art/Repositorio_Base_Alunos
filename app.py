8452e5139c349333ef9ab51d6461217c7079b225
kauanprog2-art <kauanprog2@gmail.com>
2026-06-19 13:52:30 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+musicas = {
+    "Michael Jackson": {
+        "Billie Jean": "https://www.youtube.com/watch?v=Zi_XLOBDo_Y&list=RDZi_XLOBDo_Y",
+        "Thriller": "https://www.youtube.com/watch?v=4V90AmXnguw&list=RD4V90AmXnguw",
+    },
+    "50 Cent": {
+        "In Da Club": "https://www.youtube.com/watch?v=5qm8PH4xAss&list=RD5qm8PH4xAss",
+        "Many Men": "https://www.youtube.com/watch?v=5D3crqpClPY&list=RD5D3crqpClPY",
+    },
+    "Billie Eilish": {
+        "Birds of a Feather": "https://www.youtube.com/watch?v=V9PVRfjEBTI&list=RDV9PVRfjEBTI"
+    },
+}
+
+
